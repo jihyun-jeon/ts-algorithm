aca771fe502a42266b53767a1db3823c95659c80
jihyun-jeon <jhplus13@naver.com>
2024-12-27 23:57:04 +0900

    feat:lv2.메뉴리뉴얼-해시

---
+++ 프로그래머스/해시/lv2.메뉴리뉴얼.ts
@@ -0,0 +1,18 @@
+/**
+ * 24.12.27
+ * [lv2] 메뉴 리뉴얼 - 해시
+ * https://school.programmers.co.kr/learn/courses/30/lessons/72411
+ */
+
+function solution(orders, course) {
+  var answer = [];
+  return answer;
+}
+
+console.log(
+  solution(["ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"], [2, 3, 4])
+); // ["AC", "ACDE", "BCFG", "CDE"]
+// console.log(
+//   solution(["ABCDE", "AB", "CD", "ADE", "XYZ", "XYZ", "ACD"], [2, 3, 5])
+// ); //  ["ACD", "AD", "ADE", "CD", "XYZ"]
+// console.log(solution(["XYZ", "XWY", "WXA"], [2, 3, 4])); // ["WX", "XY"]
