023ce21783aaca730adb6873eec725a5a27ca1cd
jihyun-jeon <jhplus13@naver.com>
2025-01-06 22:54:20 +0900

    feat:lv2.미로탈출-트리

---
+++ 프로그래머스/트리/lv2.미로탈출.ts
@@ -0,0 +1,13 @@
+/**
+ * 25.01.06
+ * [lv2]미로탈출 - 이진 탐색 트리
+ * https://school.programmers.co.kr/learn/courses/30/lessons/159993
+ */
+function solution(maps: string[]) {
+  var answer = 0;
+  return answer;
+}
+
+console.log(solution(["SOOOL", "XXXXO", "OOOOO", "OXXXX", "OOOOE"])); //  16
+console.log(solution(["LOOXS", "OOOOX", "OOOOO", "OOOOO", "EOOOO"])); // -1
+console.log(solution(["SOOOL", "XXXXX", "OOOOO", "OXXXX", "OOOOE"])); //  -1
