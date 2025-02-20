7a3f718d6f396f6cb6b87ea85840875bd1520aa0
jihyun-jeon <jhplus13@naver.com>
2025-02-20 23:47:40 +0900

    feat: leetcode_22.Generate_Parentheses

---
+++ 프로그래머스/백트래킹/leetcode_22.Generate_Parentheses.js
@@ -0,0 +1,26 @@
+/**
+ * 25.02.20
+ * [medium] 22. Generate Parentheses
+ * https://leetcode.com/problems/generate-parentheses/description/
+ * Topics : String, Dynamic Programming, Backtracking
+ */
+
+var generateParenthesis = function (n) {
+  const result = [];
+
+  const backtrack = (open, close, str) => {
+    if (str.length === n * 2) {
+      result.push(str);
+    }
+
+    if (open < 3) backtrack(open + 1, close, str + "(");
+    if (close < open) backtrack(open, close + 1, str + ")");
+  };
+
+  backtrack(0, 0, "");
+
+  return result;
+};
+
+console.log(generateParenthesis(3)); // ["((()))","(()())","(())()","()(())","()()()"]
+console.log(generateParenthesis(1)); // ["()"]
